67cb6769f80500c5a6a2e7ccddef36add12ef5f3
Nyanchl <rotdicm@gmail.com>
2023-11-13 11:34:47 +0900

    DBの接続方法の変更

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,20 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm'; // 追加
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmConfigService } from './config/typeorm-config.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
-    TasksModule,
-    // importsに追加
-    TypeOrmModule.forRoot({
-      type: 'postgres', // DBの種類
-      port: 5432, // 使用ポート
-      database: 'todoapp', // データベース名
-      host: 'localhost', // DBホスト名
-      username: 'daichi', // DBユーザ名
-      password: '1212', // DBパスワード
-      synchronize: true, // モデル同期(trueで同期)
-      entities: [__dirname + '/**/*.entity.{js,ts}'], // ロードするエンティティ
+    ConfigModule.forRoot({
+      envFilePath: [`.env/${process.env.NODE_ENV}.env`, '.env/default.env'],
+      isGlobal: true,
     }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useClass: TypeOrmConfigService,
+    }),
+    TasksModule,
   ],
 })
 export class AppModule {}
